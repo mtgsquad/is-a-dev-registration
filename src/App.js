614138df30c00eb784580cf71a044cddb70c6541
andrewstech <info@andrewstech.me>
2022-08-02 19:15:33 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -28,19 +28,18 @@ firebase.initializeApp({
   measurementId: "G-K9WXR60DWJ",
 });
 
-const appCheck = initializeAppCheck(app, {
-  provider: new ReCaptchaV3Provider('6LcuW0AhAAAAALp8XjrdYX0p1Kz8hXbMg5e0GIzg'),
-
-  // Optional argument. If true, the SDK automatically refreshes App Check
-  // tokens as needed.
-  isTokenAutoRefreshEnabled: true
-});
+const appCheck = require("firebase/app-check");
 
 const auth = firebase.auth();
 const githubLoginProvider = new firebase.auth.GithubAuthProvider();
 // auth.signInWithPopup(provider);
 
+appCheck.activate(
+  '6LcuW0AhAAAAALp8XjrdYX0p1Kz8hXbMg5e0GIzg',
 
+  // Optional argument. If true, the SDK automatically refreshes App Check
+  // tokens as needed.
+  true);
 
 const db = getFirestore();
 let registerPopup = false;
